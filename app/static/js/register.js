e63cb266ca8cceda7fae779e4abcd4ceae0d4377
Chrono Lux <m.sanma123@gmail.com>
2021-05-24 18:02:05 -0600

    vaildate email js

--- app/static/js/register.js
+++ app/static/js/register.js
@@ -1,11 +1,41 @@
 const usernameField = document.querySelector("#usernameField");
-const feedbackArea = document.querySelector(".invalid_feedback");
+const emailField = document.querySelector("#emailField");
+const usernamefeedbackArea = document.querySelector(".invalid_feedback");
+const emailfeedBackArea = document.querySelector(".emailFeedbackArea");
+const usernameSuccessOutput = document.querySelector(".usernameSuccessOutput");
+
+
+emailField.addEventListener("keyup", (e) => {
+    // console.log("8888888", 8888888);
+    const emailVal = e.target.value;
+    emailField.classList.remove("is-invalid");
+    emailfeedBackArea.style.display = "none";
+
+    if (emailVal.length > 0) {
+        fetch("/authentication/validate-email", {
+            body: JSON.stringify({ email: emailVal }),
+            method: "POST",
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                // console.log('data', data);
+                if (data.email_error) {
+                    emailField.classList.add("is-invalid");
+                    emailfeedBackArea.style.display = 'block';
+                    emailfeedBackArea.innerHTML = `<p>${data.email_error}</p>`;
+                }
+            });
+    }
+});
 
 usernameField.addEventListener("keyup", (e) => {
     // console.log("8888888", 8888888);
     const usernameVal = e.target.value;
+    usernameSuccessOutput.style.display = "block";
+    usernameSuccessOutput.textContent = ` Checking ${usernameVal}`;
+
     usernameField.classList.remove("is-invalid");
-    feedbackArea.style.display = 'none';
+    usernamefeedbackArea.style.display = 'none';
 
     if (usernameVal.length > 0) {
         fetch("/authentication/validate-username", {
@@ -15,12 +45,12 @@ usernameField.addEventListener("keyup", (e) => {
             .then((res) => res.json())
             .then((data) => {
                 // console.log('data', data);
+                usernameSuccessOutput.style.display = "none";
                 if (data.username_error) {
                     usernameField.classList.add("is-invalid");
-                    feedbackArea.style.display = 'block';
-                    feedbackArea.innerHTML = `<p>${data.username_error}</p>`;
+                    usernamefeedbackArea.style.display = "block";
+                    usernamefeedbackArea.innerHTML = `<p>${data.username_error}</p>`;
                 }
             });
     }
-
 });
